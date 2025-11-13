3c5c92999171d1f3ccd475faec30ee51b4bd3aa9
silvzr <117378129+silvzr@users.noreply.github.com>
2025-11-13 13:14:52 +0100

    main.sh: switch to xx's KSU
    It already supports custom managers so my signature isn't needed anymore

--- main.sh
+++ main.sh
@@ -9,12 +9,13 @@ KERNEL_GIT="https://github.com/silvzr/bootlegger_kernel_archive.git"
 KERNEL_BRANCH=""
 
 # KernelSU
-KERNELSU_REPO="tiann/KernelSU"
+KERNELSU_REPO="backslashxx/KernelSU"
+KERNELSU_BRANCH="master"
 KSU_ENABLED="false"
 
 # KernelSU Custom Manager
-MANAGER_EXPECTED_SIZE="0x354"
-MANAGER_EXPECTED_HASH="55a3dc257ef27810af6eb6e5d0b0f2ee1af1c57626dc67260abffb281db37836"
+MANAGER_EXPECTED_SIZE=""
+MANAGER_EXPECTED_HASH=""
 
 # Anykernel3
 ANYKERNEL3_GIT="https://github.com/silvzr/AnyKernel3.git"
