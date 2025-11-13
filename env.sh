14adff07699018afc6b5859cd9037a3ec6268f3e
silvzr <117378129+silvzr@users.noreply.github.com>
2025-11-13 20:39:46 +0100

    env.sh: Upgrade only essential packages
    This should take a bit less time than generic apt-get upgrade

--- env.sh
+++ env.sh
@@ -3,13 +3,12 @@
 UBUNTU_VERSION=$(lsb_release -sr)
 
 # Remove FireFox
-apt remove firefox
-apt autoremove
+apt remove firefox && apt autoremove
 
 # Environment Setup
-apt-get update && apt-get upgrade -y
+apt-get update && apt-get upgrade -y build-essential pkg-config git bc bison flex libssl-dev libelf-dev libncurses-dev python3 python3-distutils python3-venv file dwarves xz-utils zstd lz4 cpio tar rsync gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu libc6-dev-arm64-cross clang lld llvm
 
 # Install python2 if Ubuntu version is lower than 24.04
 if [[ $UBUNTU_VERSION != "24.04" ]]; then
-    apt install python2
+    apt-get install -y python2
 fi
