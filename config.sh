272db85ea9636ec112941d65fc6be4a8f1196b82
silvzr <117378129+silvzr@users.noreply.github.com>
2025-11-26 17:16:45 +0100

    Revert "config.sh: check and quit if some patch fails"
    This reverts commit c43784bdc2b7cb7865b8e5f25a8ebe95351c6b17. Not needed anymore since we added set -e in main.sh

--- config.sh
+++ config.sh
@@ -22,15 +22,6 @@ has_kprobes() {
     return 1
 }
 
-# Check for rejects
-check_patch_rejects() {
-    if find "$KERNEL_DIR" -name "*.rej" -print -quit | grep -q .; then
-        echo "ERROR: Patch application failed! Reject files found:"
-        find "$KERNEL_DIR" -name "*.rej" -exec echo "  {}" \;
-        exit 1
-    fi
-}
-
 # Avoid dirty uname
 touch $KERNEL_DIR/.scmversion
 
@@ -212,5 +203,3 @@ if [[ $KSU_ENABLED == "false" ]]; then
     SUSFS_VERSION="Disabled"
     sed -i "s/^CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=\"-$KERNEL_BRANCH-$KERNEL_NAME\"/" $DEVICE_DEFCONFIG_FILE
 fi
-
-check_patch_rejects
